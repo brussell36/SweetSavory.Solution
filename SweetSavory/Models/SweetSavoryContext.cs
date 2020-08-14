b53ce28fffc776ff4c34854ae5d009594a7a0fe2
Ben Russell <benjaminrussell36@gmail.com>
2020-08-14 08:53:03 -0700

    Add logic to SweetSavoryContext.cs

---
+++ SweetSavory/Models/SweetSavoryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SweetSavory.Models
+{
+  public class SweetSavoryContext : IdentityDbContext<AppUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treat { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public SweetSavoryContext(DbContextOptions options) : base(options) { }
+  }
+}
