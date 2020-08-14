79b555f351ffa7870ab1c0491ff1c22dc97e0554
Ben Russell <benjaminrussell36@gmail.com>
2020-08-14 09:24:42 -0700

    Add Create get and post methods to TreatsController

--- SweetSavory/Controllers/TreatsController.cs
+++ SweetSavory/Controllers/TreatsController.cs
@@ -28,5 +28,25 @@ namespace SweetSavory.Controllers
       var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
       return View(userTreats);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Taste");
+      return View();
+    }
+
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
+      _db.Treats.Add(treat);
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
